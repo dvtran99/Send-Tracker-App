43096215097190d909ae19acc8c75b662288590b
David Tran <dv.tran99@gmail.com>
2023-03-02 21:27:48 -0500

    Add dropdown box for grades

--- send_tracker_app/lib/screens/add-send-screen.dart
+++ send_tracker_app/lib/screens/add-send-screen.dart
@@ -1,6 +1,66 @@
 import 'package:flutter/material.dart';
 
-class AddSendScreen extends StatelessWidget {
+class AddSendScreen extends StatefulWidget {
+  const AddSendScreen({Key? key}) : super(key: key);
+
+  @override
+  AddSendScreenState createState() => AddSendScreenState();
+}
+
+class AddSendScreenState extends State<AddSendScreen> {
+  TextStyle textTheme = TextStyle(fontSize: 20);
+
+  String gradeValue = 'V0';
+
+  List<DropdownMenuItem<String>> get dropdownItems {
+    return [
+      DropdownMenuItem(
+        value: 'V0',
+        child: Text('V0'),
+      ),
+      DropdownMenuItem(
+        value: 'V1',
+        child: Text('V1'),
+      ),
+      DropdownMenuItem(
+        value: 'V2',
+        child: Text('V2'),
+      ),
+      DropdownMenuItem(
+        value: 'V3',
+        child: Text('V3'),
+      ),
+      DropdownMenuItem(
+        value: 'V4',
+        child: Text('V4'),
+      ),
+      DropdownMenuItem(
+        value: 'V5',
+        child: Text('V5'),
+      ),
+      DropdownMenuItem(
+        value: 'V6',
+        child: Text('V6'),
+      ),
+      DropdownMenuItem(
+        value: 'V7',
+        child: Text('V7'),
+      ),
+      DropdownMenuItem(
+        value: 'V8',
+        child: Text('V8'),
+      ),
+      DropdownMenuItem(
+        value: 'V9',
+        child: Text('V9'),
+      ),
+      DropdownMenuItem(
+        value: 'V10+',
+        child: Text('V10+'),
+      ),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,7 +71,7 @@ class AddSendScreen extends StatelessWidget {
           child: Column(
         children: [
           SizedBox(height: 20),
-          Text("Picture of Route"),
+          Text("Picture of Route", style: textTheme),
           SizedBox(height: 20),
           Container(
             height: 120,
@@ -19,9 +79,27 @@ class AddSendScreen extends StatelessWidget {
             decoration: BoxDecoration(border: Border.all(color: Colors.black)),
           ),
           SizedBox(height: 20),
-          Text("Grade"),
+          SizedBox(
+            height: 50,
+            width: 150,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Grade", style: textTheme),
+                DropdownButton(
+                    focusNode: FocusNode(canRequestFocus: false),
+                    value: gradeValue,
+                    items: dropdownItems,
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        gradeValue = newValue!;
+                      });
+                    }),
+              ],
+            ),
+          ),
           SizedBox(height: 20),
-          Text("Filters"),
+          Text("Filters", style: textTheme),
           SizedBox(height: 20),
           ElevatedButton(
               onPressed: () {
